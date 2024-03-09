2de653038281e0fd5dd1047389859e47cde1b7af
danil <shuvalov58@icloud.com>
2024-03-09 14:49:10 +0300

    добавление поле вывода колличества уроков

--- training/serializers.py
+++ training/serializers.py
@@ -3,13 +3,19 @@ from rest_framework import serializers
 from training.models import Course, Lesson
 
 
-class CourseSerializers(serializers.ModelSerializer):
-    class Meta:
-        model = Course
-        fields = '__all__'
-
-
 class LessonSerializers(serializers.ModelSerializer):
     class Meta:
         model = Lesson
+        fields = '__all__'
+
+
+class CourseSerializers(serializers.ModelSerializer):
+    lesson_count = serializers.SerializerMethodField()
+    lessons = LessonSerializers(source='lesson_set', many=True, read_only=True)
+
+    def get_lesson_count(self, instance):
+        return instance.lessons.count()
+
+    class Meta:
+        model = Course
         fields = '__all__'
